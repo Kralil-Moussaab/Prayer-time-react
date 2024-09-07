f49c70e32d3065c2cb38e59301074fc6351dcb7c
Kralil-Moussaab <moussaabkralil22@gmail.com>
2024-09-07 23:22:59 +0200

    add text center to hijri and gregorian date abd font bold in hijri weekday

--- src/components/PrayerInfo.jsx
+++ src/components/PrayerInfo.jsx
@@ -11,19 +11,19 @@ const PrayerInfo = ({
   const next = `${
     language === "ar"
       ? `: الوقت متبقي لصلاة ${nextPrayerName} `
-      : `There is time left for the ${nextPrayerName} prayer`
+      : `There is time left for the ${nextPrayerName} prayer :`
   }`;
   return (
     <>
       <div className="flex flex-col items-center mb-10">
-        <p className="text-5xl mb-3">{hijriWeekday}</p>
+        <p className="text-6xl font-bold mb-3">{hijriWeekday}</p>
         <p className="text-4xl mb-3 text-center">{next}</p>
         <p className="font-black text-5xl sm:text-6xl text-center">
           {timeLeft}
         </p>
         <div className="flex gap-5 mt-3">
-          <p className="font-bold text-lg">{gregorianDate}</p>
-          <p className="font-black text-lg">{hijriDate}</p>
+          <p className="font-bold text-center text-lg">{gregorianDate}</p>
+          <p className="font-black text-center text-lg">{hijriDate}</p>
         </div>
       </div>
     </>
